a05e1f54dc61e1c0844ebcbce108b4fa79fa1ad1
Imran Nazir <imran.web.developer2021@gmail.com>
2023-06-14 11:36:52 +0600

    add API

--- apps/server/dist/app.module.js
+++ apps/server/dist/app.module.js
@@ -8,15 +8,48 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
+const core_1 = require("@nestjs/core");
+const mongoose_1 = require("@nestjs/mongoose");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const http_exception_filter_1 = require("./common/filter/http-exception.filter");
+const mobile_module_1 = require("./common/modules/mobile.module");
+const users_module_1 = require("./common/modules/users.module");
+const auth_module_1 = require("./common/modules/auth.module");
+const comment_module_1 = require("./common/modules/comment.module");
+const utils_module_1 = require("./common/modules/utils.module");
+const upload_module_1 = require("./common/modules/upload.module");
+const jwt_1 = require("@nestjs/jwt");
+const Axios_module_1 = require("./common/modules/Axios.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            config_1.ConfigModule.forRoot(),
+            jwt_1.JwtModule.registerAsync({
+                useFactory: () => ({
+                    secret: process.env.CLIENT_SECRETE,
+                }),
+            }),
+            mongoose_1.MongooseModule.forRoot(process.env.DB),
+            Axios_module_1.AxiosModule,
+            mobile_module_1.MobileModule,
+            users_module_1.UsersModule,
+            auth_module_1.AuthModule,
+            comment_module_1.CommentModule,
+            utils_module_1.UtilsModule,
+            upload_module_1.UploadModule,
+        ],
         controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        providers: [
+            app_service_1.AppService,
+            {
+                provide: core_1.APP_FILTER,
+                useClass: http_exception_filter_1.HttpExceptionFilter,
+            },
+        ],
     })
 ], AppModule);
 exports.AppModule = AppModule;
--- apps/server/dist/main.js
+++ apps/server/dist/main.js
@@ -2,9 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const helmet_1 = require("helmet");
+const validation_pipe_1 = require("./common/pipes/validation.pipe");
+const admin = require("firebase-admin");
+const configuration_1 = require("./config/configuration");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(5000);
+    app.enableCors();
+    app.use((0, helmet_1.default)());
+    app.useGlobalPipes(new validation_pipe_1.ValidationPipe());
+    app.setGlobalPrefix('api');
+    const { firebase, storageBucket } = (0, configuration_1.config)();
+    admin.initializeApp({
+        credential: admin.credential.cert(firebase),
+        storageBucket: storageBucket,
+    });
+    await app.listen(process.env.PORT || 5000, async () => {
+        console.log('listening on port ' + (await app.getUrl()));
+    });
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
